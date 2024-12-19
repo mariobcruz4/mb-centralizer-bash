8a5575deeacc24487d3aff1f5ca911306924abef
AzureAD\MarioBautistaCruz <bautistac.mario@gmail.com>
2024-12-19 13:43:31 -0600

    introducing swagger-ui

--- swagger-ui-bundle.js
+++
@@ -1,2 +0,0 @@
-// swagger-ui-bundle.js (descargado o referenciado desde CDN)
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui-bundle.min.js"></script>
--- swagger-ui-standalone-preset.js
+++
@@ -1,2 +0,0 @@
-// swagger-ui-standalone-preset.js (descargado o referenciado desde CDN)
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.52.5/swagger-ui-standalone-preset.min.js"></script>
