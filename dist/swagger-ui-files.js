8c1ae15e5323518d6196e884eadafdbf290386c9
AzureAD\MarioBautistaCruz <bautistac.mario@gmail.com>
2024-12-31 14:37:10 -0600

    github centralizer

--- dist/swagger-ui-files.js
+++ dist/swagger-ui-files.js
@@ -2,5 +2,5 @@ window.swaggerUrls = [
  {url: "./swagger-specs/swagger.yaml", name: "Meters"},
  {url: "./swagger-specs/swagger-cars.json", name: "Cars"},
  {url: "./swagger-specs/music/dualipa-songs.json", name: "Dua Lipa"},
- {url: "./swagger-specs/music/coldplay.json", name: "Coldplay"}
-, {url: "./swagger-specs/music/newswagger.json", name: "New API"}, {url: "./swagger-specs/music/newswagger.json", name: "New API"}];
+ {url: "./swagger-specs/music/coldplay.json", name: "Coldplay"},
+ {url: "./swagger-specs/music/newswagger.json", name: "New API"}];
